3c00e3e85cf377e7b2e245f6b2a1bc339fdd2d39
Jostautas <jostautas.sakas@gmail.com>
2022-11-28 13:29:12 +0200

    tree generated

--- Program/ConsoleApp1/Program.cs
+++ Program/ConsoleApp1/Program.cs
@@ -5,5 +5,16 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("hi");
+
+        Tree tree = new();
+
+        tree.AddNewNode(0);
+        tree.AddNewNode(0);
+        tree.AddNewNode(0);
+        tree.AddNewNode(1);
+        tree.AddNewNode(1);
+
+        tree.PrintTree();
+
     }
 }
---
+++ Program/ConsoleApp1/Tree.cs
@@ -0,0 +1,36 @@
+﻿namespace Program;
+
+public class Tree
+{
+    private int Total { get; set; }
+    private List<Node> Nodes { get; set; }
+
+    public Tree()
+    {
+        Total = 0;
+        Nodes = new List<Node>();
+    }
+
+    public void AddNewNode(int addTo)
+    {
+        Nodes.Add(new Node(Total, addTo));
+        ++Total;
+    }
+
+    public void PrintTree()
+    {
+        for(int i = 0; i < Nodes.Count; i++)
+        {
+            Console.Write(Nodes[i].Id + ": ");
+
+            for(int j = 0; j < Nodes.Count; j++)
+            {
+                if (Nodes[j].Previous == Nodes[i].Id)
+                {
+                    Console.Write(Nodes[j].Id + ", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
