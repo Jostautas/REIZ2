2ddf8e4ce32f18edc14c3b720d0d503f5de3dde4
Jostautas <jostautas.sakas@gmail.com>
2022-11-28 18:51:15 +0200

    GetDepth works

--- Program/ConsoleApp1/Program.cs
+++ Program/ConsoleApp1/Program.cs
@@ -4,17 +4,45 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("hi");
+        Example();
 
+    }
+
+    public static void Example()
+    {
         Tree tree = new();
 
-        tree.AddNewNode(0);
-        tree.AddNewNode(0);
-        tree.AddNewNode(0);
-        tree.AddNewNode(1);
-        tree.AddNewNode(1);
+        tree.AddNewNode(0); // id 1
+        tree.AddNewNode(0); // id 2
 
-        tree.PrintTree();
+        tree.AddNewNode(1); // id 3
 
+        tree.AddNewNode(2); // id 4
+        tree.AddNewNode(2); // id 5
+        tree.AddNewNode(2); // id 6
+
+        tree.AddNewNode(4); // id 7
+
+        tree.AddNewNode(5); // id 8
+        tree.AddNewNode(5); // id 9
+
+        tree.AddNewNode(8); // id 10
+
+        /*
+                0
+               / \
+              1   2
+             /   /|\
+            3   4 5 6
+               /  /\     
+              7  8  9
+                 |
+                 10
+         */
+
+
+        tree.PrintTree(); // see tree connections in text
+
+        tree.PrintDepth();
     }
 }
--- Program/ConsoleApp1/Tree.cs
+++ Program/ConsoleApp1/Tree.cs
@@ -5,10 +5,12 @@ public class Tree
     private int Total { get; set; }
     private List<Node> Nodes { get; set; }
 
-    public Tree()
+    public Tree()   // Root (0'th) node is created by default
     {
-        Total = 0;
         Nodes = new List<Node>();
+        Nodes.Add(new Node(0, 0));
+        Total = 1;
+        Depth = 1;
     }
 
     public void AddNewNode(int addTo)
@@ -25,12 +27,39 @@ public class Tree
 
             for(int j = 0; j < Nodes.Count; j++)
             {
-                if (Nodes[j].Previous == Nodes[i].Id)
+                if (Nodes[j].Previous == Nodes[i].Id && Nodes[i].Id != Nodes[j].Id)
                 {
                     Console.Write(Nodes[j].Id + ", ");
                 }
             }
             Console.WriteLine();
         }
+    }
+
+    public void PrintDepth()
+    {
+        Console.WriteLine("Depth of given tree is " + GetDepth(0, 1));
+    }
+
+    public int GetDepth(int startAt, int depth)    // start search at node [startAt]
+    {
+        List<int> list = new();
+
+        if(list.Count == 0)
+        {
+            list.Add(depth);
+        }
+
+        for (int i = 0; i < Nodes.Count; i++)
+        {
+            if ( (Nodes[startAt].Id == Nodes[i].Previous)
+                && (startAt != i) )
+            {
+                int tempDepth = GetDepth(i, depth+1);
+
+                list.Add(tempDepth);
+            }
+        }
+        return list.Max();
     }
 }
